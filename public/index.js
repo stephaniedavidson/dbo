c7470830a54d0945388059fad3b6b86d11c626e3
Steph Davidson <stephcdavidson@gmail.com>
2021-08-16 23:16:50 -0400

    sfkjdflgj

--- public/index.js
+++ public/index.js
@@ -1,37 +1,74 @@
 import * as THREE from "https://cdn.skypack.dev/three";
-
-import confetti from "https://cdn.skypack.dev/canvas-confetti";
-confetti();
-window.addEventListener("click", () => {
-    confetti();
-});
-
 let camera, scene, renderer;
 let geometry, material, mesh;
 
 init();
 
 function init() {
+    const threeDiv = document.getElementById("three");
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
     camera.position.z = 1;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
     material = new THREE.MeshNormalMaterial();
 
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(threeDiv.getBoundingClientRect().width, threeDiv.getBoundingClientRect().height);
+    // renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animation);
-    document.body.appendChild(renderer.domElement);
+    threeDiv.appendChild(renderer.domElement);
 }
 
 function animation(time) {
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
+    mesh.rotation.y = time / 3000;
 
     renderer.render(scene, camera);
+}
+
+//*******************************************//
+//*******************************************//
+//*******************************************//
+
+dragElement(document.getElementById("m0dal"));
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id)) {
+        document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
