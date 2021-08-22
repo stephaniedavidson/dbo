0b6001c8bd62525fa9b84715ad78d2f432c5d586
Steph Davidson <stephcdavidson@gmail.com>
2021-08-22 18:22:39 -0400

    webgl organize

--- public/index.js
+++ public/index.js
@@ -1,38 +1,85 @@
 import * as THREE from "https://cdn.skypack.dev/three";
-let camera, scene, renderer;
-let geometry, material, mesh;
+import threejsOrbitControls from "https://cdn.skypack.dev/threejs-orbit-controls";
 
-init();
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
 
-function init() {
-    const threeDiv = document.getElementById("three");
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-    camera.position.z = 1;
+// Scene
+const scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
+// Object
 
-    geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    material = new THREE.MeshNormalMaterial();
+const geometry = new THREE.BufferGeometry();
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(threeDiv.getBoundingClientRect().width, threeDiv.getBoundingClientRect().height);
-    renderer.setAnimationLoop(animation);
-    threeDiv.appendChild(renderer.domElement);
-
-    const windowResize = () => {
-        renderer.setSize(threeDiv.getBoundingClientRect().width, threeDiv.getBoundingClientRect().height);
-    };
-    window.addEventListener("resize", windowResize);
+const count = 50;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = (Math.random() - 0.5) * 5;
 }
 
-function animation(time) {
-    mesh.rotation.y = time / 3000;
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAttribute);
 
+const material = new THREE.MeshBasicMaterial({ color: 0xf6f93f, wireframe: true });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new threejsOrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update controls
+    controls.update();
+
+    // Render
     renderer.render(scene, camera);
-}
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
 
 //*******************************************//
 //*******************************************//
